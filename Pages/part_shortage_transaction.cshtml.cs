3358807babdae91a5ef88bd5996374a7ffe13f48
pavithran064 <pavithran.vv@njsinfotech.com>
2023-12-14 12:32:02 +0530

    Add project files.

---
+++ Pages/part_shortage_transaction.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+public class part_shortage_transactionModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
